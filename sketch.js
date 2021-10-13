b590cb23cd0d6358e3690755f421d25b9c582da7
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-10-13 17:26:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 var ball;
-var vx = 2;
+var vx = 5;
 
 function setup() {
   createCanvas(400,400);
 
-  ball = createSprite(100,100,100,100);
+  ball = createSprite(100,100,50,50);
 }
 
 function draw() 
 {
   background(51);
-  if(ball.position.x<=0 || ball.position.x>=width)
+  if(ball.position.x<=25 || ball.position.x>=width-25)
   {
-    vx= -vx;
+    vx = -vx;
   }
   ball.velocity.x = vx;
 
